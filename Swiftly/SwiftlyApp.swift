aa2aabcac149a73cc902636e975d8b2e18b0048e
Patrick <patrickpc1015@gmail.com>
2024-10-25 17:51:00 -0700

    Initial Commit

---
+++ Swiftly/SwiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyApp.swift
+//  Swiftly
+//
+//  Created by Patrick on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
